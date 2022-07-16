d30976bb1f2205b756abc9536847748d1d7dd043
Tomislav Milkovic <tomislav.m.milkovic@gmail.com>
2022-07-16 01:02:13 +0100

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// @ts-ignore
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <App />
+);
